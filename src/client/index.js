b130b2850ab1f9e638a3e2bd1180d561b83eec5c
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-03-11 23:40:13 -0800

    feat: Add SCSS related codes to webpack.config files

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
 
 console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+alert('I EXISTed!');
+console.log('CHANGE!!');
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,33 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+	entry: './src/client/index.js',
+	mode: 'production',
+	optimization: {
+		minimizer: [new TerserPlugin({}), new CssMinimizerPlugin()],
+	},
+	module: {
+		rules: [
+			{
+				test: '/.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/client/views/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin(),
+	],
+};
